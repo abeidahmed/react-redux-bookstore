ae61032746a463cb7018956ce06a6d3250641337
abeidahmed <abeidahmed92@gmail.com>
2021-04-14 12:20:04 +0530

    List all books

---
+++ src/components/Book.js
@@ -0,0 +1,9 @@
+const Book = () => (
+  <tr>
+    <td>123</td>
+    <td>title</td>
+    <td>category</td>
+  </tr>
+);
+
+export default Book;
--- src/containers/BooksList.js
+++ src/containers/BooksList.js
@@ -1,7 +1,18 @@
+import Book from '../components/Book';
+
 const BooksList = () => (
-  <div>
-    <h1>hell owrold</h1>
-  </div>
+  <table>
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Category</th>
+      </tr>
+    </thead>
+    <tbody>
+      <Book />
+    </tbody>
+  </table>
 );
 
 export default BooksList;
