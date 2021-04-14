ed0cd65698db06a714bd1c7860ad05850accffbc
abeidahmed <abeidahmed92@gmail.com>
2021-04-14 12:13:44 +0530

    Make components/containers

---
+++ src/containers/BookForm.js
@@ -0,0 +1,7 @@
+const BookForm = () => (
+  <div>
+    <h1>hell owrold</h1>
+  </div>
+);
+
+export default BookForm;
---
+++ src/containers/BooksList.js
@@ -0,0 +1,7 @@
+const BooksList = () => (
+  <div>
+    <h1>hell owrold</h1>
+  </div>
+);
+
+export default BooksList;
