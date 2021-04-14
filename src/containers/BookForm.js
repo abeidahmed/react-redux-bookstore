26f613c4efc6db6ed1a5565012b3d9ccea22ea9e
abeidahmed <abeidahmed92@gmail.com>
2021-04-14 12:28:51 +0530

    List bookform options

--- src/containers/BookForm.js
+++ src/containers/BookForm.js
@@ -1,7 +1,31 @@
-const BookForm = () => (
-  <div>
-    <h1>hell owrold</h1>
-  </div>
-);
+const BookForm = () => {
+  const categories = [
+    'Action',
+    'Biography',
+    'History',
+    'Horror',
+    'Kids',
+    'Learning',
+    'Sci-Fi',
+  ];
+
+  return (
+    <div>
+      <form>
+        <div>
+          <input type="text" id="title" />
+          <select>
+            {categories.map((category, index) => (
+              // eslint-disable-next-line react/no-array-index-key
+              <option value={category} key={index}>
+                {category}
+              </option>
+            ))}
+          </select>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default BookForm;
